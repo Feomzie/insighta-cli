e3875603df34002b508417d5071fbf112fbac3f9
Feomzie <joyogboh@gmail.com>
2026-04-29 23:18:36 +0100

    feat: Remove redundant name parameter from auth command registration

--- insighta/main.py
+++ insighta/main.py
@@ -3,7 +3,7 @@ from insighta.commands import auth, profiles
 
 app = typer.Typer()
 
-app.add_typer(auth.app, name="auth")
+app.add_typer(auth.app)
 app.add_typer(profiles.app, name="profiles")
 
 if __name__ == "__main__":
